f31a8f67146f4d74a29a5da10c90272864abe031
tiepkhactran <tiepkhactran@users.noreply.github.com>
2019-12-16 22:28:05 +0700

    date

---
+++ DateRangePicker/Controllers/DateRangePickerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DateRangePickerDemo.Controllers
+{
+    public class DateRangePickerController : Controller
+    {
+        // GET: DateRangePicker
+        public ActionResult Index(DateTime? start,DateTime? end)
+        {
+            ViewBag.StartDate = start;
+            ViewBag.EndDate = end;
+            return View();
+        }
+
+    }
+}
---
+++ DateRangePicker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DateRangePicker.MvcApplication" Language="C#" %>
